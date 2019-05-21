b09cef54d57294a6eb999a7758bfc9b7505b90e1
Thomas Bo Nielsen <skt.thomas@gmail.com>
2019-05-21 12:08:03 +0200

    TIlføjet source til WebSecurityConfig
    Fortæller hvor vi har fået den fra, samt hvad den gør

--- src/main/java/com/dat18v1swd3/planner/WebSecurityConfig.java
+++ src/main/java/com/dat18v1swd3/planner/WebSecurityConfig.java
@@ -1,5 +1,10 @@
 package com.dat18v1swd3.planner;
 
+/*Source: https://stackoverflow.com/questions/50486314/how-to-solve-403-error-in-spring-boot-post-request/50491615
+*
+* Denne config-fil tillader os at tilgå databasen uden at trigge Spring security's Anti-CORS procedure
+* */
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
